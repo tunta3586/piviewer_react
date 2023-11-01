3a04d080a27daa88afac714147abe8fffdeb6bd1
a024148 <tunta3586@gmail.com>
2023-11-01 23:14:06 +0900

    로그인 레이아웃 추가

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import App from './App';
-import Header from './components/Header'
+import MainContents from './components/MainContents.js';
+import Header from './components/Header';
 
 const headerTag = ReactDOM.createRoot(document.getElementById('header'));
 headerTag.render(
-  <React.StrictMode>
-    <Header />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Header />
+    </React.StrictMode>
 );
 
 const contents = ReactDOM.createRoot(document.getElementById('contents'));
 contents.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MainContents />
 );
